2c03d8d76dfc63cbd24ad1397bbdc13d78825b43
RodrigoGaona <huelum63@gmail.com>
2025-05-03 14:54:17 -0600

    VSCODE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5163/api/movies") // Ajusta el puerto si es diferente
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error("Error fetching movies:", error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +27,15 @@ function App() {
         >
           Learn React
         </a>
+
+        <div>
+          <h2>Lista de Películas</h2>
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>{movie.title}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
